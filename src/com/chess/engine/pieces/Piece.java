eb4aced7ebbca77f570cab4e1e6700f63bbb0392
Kevin Bowe <kevinbowe1999@gmail.com>
2019-09-18 01:08:33 -0400

    HashCode and Equals for Piece class
    Overwrote the equals and the hashcode method for the Piece class
    
    *Read up on HashCode I didn't know what I was doing*

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -12,6 +12,7 @@ public abstract class Piece {
 	protected final int piecePosition;
 	protected final Alliance pieceAlliance;
 	protected final boolean isFirstMove;
+	private final int cacheHashCode;
 	
 	Piece(final int piecePosition, 
 		  final Alliance pieceAlliance,
@@ -21,6 +22,34 @@ public abstract class Piece {
 		//TODO more work
 		this.isFirstMove = false;
 		this.pieceType = pieceType;
+		this.cacheHashCode = computeHashCode();
+	}
+	
+	private int computeHashCode() {
+		int result = pieceType.hashCode();
+		result = 31 * result + pieceAlliance.hashCode();
+		result = 31 * result + piecePosition;
+		result = 31 * result + (isFirstMove ? 1 : 0);
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object other) {
+		if(this == other) {
+			return true;
+		}
+		if(!(other instanceof Piece)) {
+			return false;
+		}
+		final Piece otherPiece = (Piece) other;
+		return pieceAlliance == otherPiece.getPieceAlliance() && pieceType == otherPiece.getPieceType() &&
+			   piecePosition == otherPiece.getPiecePosition() && isFirstMove == otherPiece.isFirstMove();	
+				
+	}
+	
+	@Override 
+	public int hashCode() {
+		return this.cacheHashCode;
 	}
 	
 	public Alliance getPieceAlliance() {
