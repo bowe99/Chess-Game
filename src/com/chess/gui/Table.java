d009c2a3cd7128492f97b71a0f06508141387314
Kevin Bowe <kevinbowe1999@gmail.com>
2019-09-20 00:05:27 -0400

    Added the actual Board to the Frame
    The chess board is now visible using the assignTileColorMethod
    
    Created a boolean array for each row of the board to create the pattern
    in the board
    
    Added an exit item to the menu bar

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,6 +1,7 @@
 package com.chess.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -25,6 +26,9 @@ public class Table {
 	private static final Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
 	private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
 	private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
+	
+	private final Color lightTileColor = Color.decode("#FFFACD");
+    private final Color darkTileColor = Color.decode("#593E1A");
 	
 	public Table() {
 		this.gameFrame = new JFrame("JChess");
@@ -64,7 +68,19 @@ public class Table {
 			
 		});
 		
+		final JMenuItem exitMenuItem = new JMenuItem("Exit");
+		
+		exitMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+			
+		});
 		fileMenu.add(openPGN);
+		fileMenu.add(exitMenuItem);
+		
 		return fileMenu;
 	}
 	
@@ -103,8 +119,18 @@ public class Table {
 		}
 
 		private void assignTileColor() {
-			// TODO Auto-generated method stub
-			
+			if(BoardUtils.FIRST_ROW[this.tileID] ||
+					BoardUtils.THIRD_ROW[this.tileID] ||
+					BoardUtils.FIFTH_ROW[this.tileID] ||
+					BoardUtils.SEVENTH_ROW[this.tileID]) {
+				setBackground(this.tileID % 2 == 0 ? lightTileColor : darkTileColor);
+			}
+			else if(BoardUtils.SECOND_ROW[this.tileID] ||
+					BoardUtils.FOURTH_ROW[this.tileID] ||
+					BoardUtils.SIXTH_ROW[this.tileID] ||
+					BoardUtils.EIGHTH_ROW[this.tileID]) {
+				setBackground(this.tileID % 2 != 0 ? lightTileColor : darkTileColor);
+			}
 		}
 		
 		
