7253b608c03c3d47bbe171269628a38e63b2aa0a
Kevin Bowe <kevinbowe1999@gmail.com>
2019-09-19 23:48:21 -0400

    Added the methods and classes that will create Board
    Created classes BoardPanel and Tile Panel which will be used to display
    the board

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,36 +1,53 @@
 package com.chess.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+import com.chess.engine.board.BoardUtils;
 
 public class Table {
 	
 	private final JFrame gameFrame;
+	private final BoardPanel boardPanel;
 	
 	private static final Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
+	private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
+	private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
 	
 	public Table() {
 		this.gameFrame = new JFrame("JChess");
-		final JMenuBar tableMenuBar = new JMenuBar();
-		populateMenuBar(tableMenuBar);
+		this.gameFrame.setLayout(new BorderLayout());
+		JMenuBar tableMenuBar = createTableMenuBar();
+		this.gameFrame.setJMenuBar(tableMenuBar);
 		this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+		
+		this.boardPanel = new BoardPanel();
+		this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
+		
 		this.gameFrame.setVisible(true);
 		
-		this.gameFrame.setJMenuBar(tableMenuBar);
 		
 		
 		
 	}
 	
 	
-	private void populateMenuBar(final JMenuBar tableMenuBar) {
+	private JMenuBar createTableMenuBar() {
+		final JMenuBar tableMenuBar = new JMenuBar();
 		tableMenuBar.add(createFileMenu());
+		return tableMenuBar; 
 	}
 
 
@@ -50,4 +67,67 @@ public class Table {
 		fileMenu.add(openPGN);
 		return fileMenu;
 	}
+	
+	
+	private class BoardPanel extends JPanel{
+		
+		final List<TilePanel> boardTiles;
+		
+		BoardPanel(){
+			super(new GridLayout(8, 8));
+			this.boardTiles = new ArrayList<>();
+			
+			for(int i = 0; i < BoardUtils.NUM_TILES; i++) {
+				final TilePanel tilePanel = new TilePanel(this, i);
+				this.boardTiles.add(tilePanel);
+				add(tilePanel);
+			}
+			
+			setPreferredSize(BOARD_PANEL_DIMENSION);
+			validate();
+		}
+		
+	}
+	
+	private class TilePanel extends JPanel{
+		
+		private final int tileID;
+		
+		TilePanel(final BoardPanel boardPanel, 
+				  final int tileID){
+			super(new GridBagLayout());
+			this.tileID = tileID;
+			setPreferredSize(TILE_PANEL_DIMENSION);
+			assignTileColor();
+			validate();
+		}
+
+		private void assignTileColor() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
